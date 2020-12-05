2bd3f2d9d2e75b4d711d945835247ded3c4802e9
unknown <nkhlkamboj@gmail.com>
2020-12-05 01:32:44 -0800

    Custome rect build with color and size update

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var redButton = findViewById<Button>(R.id.redButton)
+        var greenButton = findViewById<Button>(R.id.greenButton)
+
+        var increase = findViewById<Button>(R.id.increase)
+        var decrease = findViewById<Button>(R.id.decrease)
+
+
+        var rangeSeekBar = findViewById<RangeSeekBar>(R.id.rangeSeekBar)
+        redButton.setOnClickListener {
+            rangeSeekBar.updateColor(Color.RED)
+        }
+        greenButton.setOnClickListener {
+            rangeSeekBar.updateColor(Color.GREEN)
+        }
+
+        increase.setOnClickListener {
+            rangeSeekBar.updateSize(0f,0f,0f,0f)
+        }
+
+        decrease.setOnClickListener {
+            rangeSeekBar.updateSize(50f,50f,50f,50f)
+        }
+
     }
 }
+
+
---
+++ MyApplication/app/src/main/java/com/example/myapplication/RangeSeekBar.kt
@@ -0,0 +1,79 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Paint.ANTI_ALIAS_FLAG
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+
+class RangeSeekBar(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    var mShowText = false
+    var textPos = 0;
+    var xx = 0f
+    var yy = 0f
+    var w = 0f
+    var h = 0f
+
+    var rectBodyColor = Color.RED
+
+    init {
+        context?.theme?.obtainStyledAttributes(
+            attrs,
+            R.styleable.RangeSeekBar,
+            0, 0)?.apply {
+
+            try {
+                mShowText = getBoolean(R.styleable.RangeSeekBar_showText, false)
+                textPos = getInteger(R.styleable.RangeSeekBar_labelPosition, 0)
+            } finally {
+                recycle()
+            }
+         }
+
+        Log.v("SHOW_TEXT",""+mShowText);
+        Log.v("SHOW_TEXT",""+textPos);
+    }
+
+     fun updateColor(color: Int)
+    {
+        rectBodyColor = color;
+       invalidate()
+       requestLayout()
+    }
+
+
+    fun updateSize(x: Float,y:Float,w:Float,h:Float)
+    {
+
+        this.xx = x
+        this.yy = y
+        this.w = w
+        this.h = h
+        invalidate()
+        requestLayout()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+
+        Log.v("SHOW_TEXT","drawing");
+
+
+         val reactPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            style = Paint.Style.FILL
+             color = rectBodyColor
+
+        }
+
+        canvas.apply {
+            drawRect(xx, yy, getWidth().toFloat()-w, getHeight().toFloat()-h, reactPaint );
+
+        }
+    }
+
+}
