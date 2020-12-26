57e8383ded1da8061d31847757e3deecc3a2e2f0
unknown <nkhlkamboj@gmail.com>
2020-12-26 03:53:38 -0800

    thum and bar added

--- MyApplication/app/src/main/java/com/example/myapplication/RangeSeekBar.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/RangeSeekBar.kt
@@ -1,13 +1,14 @@
 package com.example.myapplication
 
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.Paint.ANTI_ALIAS_FLAG
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+
 
 class RangeSeekBar(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
@@ -17,14 +18,20 @@ class RangeSeekBar(context: Context?, attrs: AttributeSet?) : View(context, attr
     var yy = 0f
     var w = 0f
     var h = 0f
+    var thumRadius = 0f;
 
     var rectBodyColor = Color.RED
+    var circleBodyColor = Color.GREEN
+    var thumbBarHeight = 0f;
 
     init {
         context?.theme?.obtainStyledAttributes(
             attrs,
             R.styleable.RangeSeekBar,
             0, 0)?.apply {
+
+            thumRadius =  getResources().getDimension(R.dimen.thumb_radius).toInt().toFloat()
+            thumbBarHeight = getResources().getDimension(R.dimen.thumb_bar_height).toInt().toFloat()
 
             try {
                 mShowText = getBoolean(R.styleable.RangeSeekBar_showText, false)
@@ -57,21 +64,73 @@ class RangeSeekBar(context: Context?, attrs: AttributeSet?) : View(context, attr
         requestLayout()
     }
 
+
+    private fun measureDimension(desiredSize: Int, measureSpec: Int): Int {
+        var result: Int
+        val specMode = MeasureSpec.getMode(measureSpec)
+        val specSize = MeasureSpec.getSize(measureSpec)
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize
+            Log.e("ChartView", "Exactly")
+        } else {
+            Log.e("ChartView", "NotExact")
+            result = desiredSize
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize)
+//                if(specSize>result)
+//                    result = specSize
+            }
+
+        }
+        if (result < desiredSize) {
+            Log.e("ChartView", "The view is too small, the content might get cut")
+        }
+        return result
+    }
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        Log.v("Chart onMeasure w", MeasureSpec.toString(widthMeasureSpec))
+        Log.v("Chart onMeasure h", MeasureSpec.toString(heightMeasureSpec))
+        var desiredWidth = 0
+        var desiredHeight = 0
+
+        getResources().apply {desiredWidth =
+            (getDimension(R.dimen.range_seekbar_width)+
+            getDimension(R.dimen.range_seekbar_padding_left)+
+            getDimension(R.dimen.range_seekbar_padding_right)).toInt()}
+
+        getResources().apply {desiredHeight =
+            (getDimension(R.dimen.range_seekbar_height)+
+               getDimension(R.dimen.range_seekbar_padding_top)+
+                    getDimension(R.dimen.range_seekbar_padding_bottom)).toInt()}
+
+                setMeasuredDimension(
+            measureDimension(desiredWidth, widthMeasureSpec),
+            measureDimension(desiredHeight, heightMeasureSpec)
+        )
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
-
         Log.v("SHOW_TEXT","drawing");
-
-
          val reactPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
             style = Paint.Style.FILL
              color = rectBodyColor
 
         }
 
+        val circlePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            style = Paint.Style.FILL
+            color = circleBodyColor
+
+        }
+
         canvas.apply {
-            drawRect(xx, yy, getWidth().toFloat()-w, getHeight().toFloat()-h, reactPaint );
+            //drawRect(xx, yy, getWidth().toFloat()-w, getHeight().toFloat()-h, reactPaint );
+            drawRect(xx, getHeight()/2f-thumbBarHeight, getWidth().toFloat()-w, getHeight()/2f+thumbBarHeight, reactPaint );
+            drawCircle(w+thumRadius, getHeight()/2f, thumRadius, circlePaint);
+            //drawCircle(width.toFloat()-thumRadius+w, getHeight()/2f, thumRadius, circlePaint);
 
         }
     }
